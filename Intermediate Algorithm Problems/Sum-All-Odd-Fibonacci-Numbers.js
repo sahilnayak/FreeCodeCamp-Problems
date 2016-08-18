0453da441660277a1ce05967765c69c9ff592f69
sahilnayak <snayak.researcher@gmail.com>
2016-08-18 16:27:38 -0700

    Sum of Odd Fibonacci numbers
    Given a positive integer num, return the sum of all odd Fibonacci
    numbers that are less than
    or equal to num.

--- Intermediate Algorithm Problems/Sum-All-Odd-Fibonacci-Numbers.js
+++ Intermediate Algorithm Problems/Sum-All-Odd-Fibonacci-Numbers.js
@@ -1,9 +1,17 @@
 /* Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than 
 or equal to num. */
 
-
 function sumFibs(num) {
-  return num;
+var arr = [1]
+
+for(var i =1; i<=num;){
+	arr.push(i)
+	i = arr[arr.length-2] + arr[arr.length-1]
+}
+return arr.reduce(function(acc,item){
+		if(item%2!==0) return acc + item
+		else return acc
+	});
 }
 
 
