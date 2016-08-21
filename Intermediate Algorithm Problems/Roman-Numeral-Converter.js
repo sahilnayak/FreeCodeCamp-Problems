a4e04458a6790c6169469a8e14c09b825298be12
sahilnayak <snayak.researcher@gmail.com>
2016-08-20 17:26:00 -0700

    Roman Number Converter
    Convert the given number into a roman numeral.

--- Intermediate Algorithm Problems/Roman-Numeral-Converter.js
+++ Intermediate Algorithm Problems/Roman-Numeral-Converter.js
@@ -2,9 +2,16 @@
 
 function convertToRoman(num) {
 
+let obj = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+let newString = ''
 
-
- return num;
+for(var keys in obj) {
+	while(num>=obj[keys]){
+		newString += keys
+		num -= obj[keys]
+	}
+}
+ return newString;
 }
 
 
